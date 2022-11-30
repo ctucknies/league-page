beae593e6fdfb2243a0f6dc7e7eba5799f015604
ctucknies <88265694+ctucknies@users.noreply.github.com>
2022-11-29 20:57:10 -0800

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,7 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Average Joes. Not above average, not below average. Just Average Joes.</p>
+  <p>Average Joes. Not above average, not below average. Just Average.</p>
 `;
 
 /*   STEP 3   */
